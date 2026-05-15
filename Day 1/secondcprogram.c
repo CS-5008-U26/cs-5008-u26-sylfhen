cf9d061298d356f38b7e8613d383cc644bf5caa7
Sylfhen <mcleod.sy@northeastern.edu>
2026-05-15 16:53:01 -0700

    Excluded char t[100] since t is not used elsewhere in the code body.

--- Day 1/secondcprogram.c
+++ Day 1/secondcprogram.c
@@ -3,10 +3,12 @@
 
 
 int main() {
-    char s[100];
-    char t[100];
+    char s[100]; 
+
+    /* excluded char t[100] since it is not used elsewhere in the code */
+    
     printf ("What is your name? ");
     scanf("%s", s);
     fgets(s,100,stdin);
-    printf("%s? That's a funny name!",t);
+    printf("%s? That's a funny name!",s);
 }

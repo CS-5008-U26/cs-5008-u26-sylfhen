abde3c20b3dec6b9babb0426e77c7875f7dfb835
Sylfhen <mcleod.sy@northeastern.edu>
2026-05-15 17:09:29 -0700

    Copied over firstcprogram to thirdcprogram for modification

--- Day 1/thirdcprogram.c
+++ Day 1/thirdcprogram.c
@@ -1,6 +1,37 @@
+/* Standard Libraries */
 #include <stdio.h>
 
+/**** MAIN PROGRAM ****/
+
 int main() {
-    printf("This is the third C program.\n");
+
+    int x; 
+    int y;   
+    int sum;                      // initialize sum
+    char s[100];                    // s is used to "pause" the console so it doesn't exit too quickly
+
+    /* Ask for a number and print it out */
+    printf("Enter a number: ");     // prompt the user
+    scanf("%d", &x);                // read a number from the console ("stdin")
+    fgets(s,100,stdin);             // sadly, we have to remove the \n that scanf leaves in stdin
+    printf("You entered %d\n", x);  // tell the user what number they just entered
+    
+    /* Normally I would not put a comment on every line of code.
+       I just did it above because you are learning C for the first time.
+       I would only put a comment on a line if it was hard to understand or required that you
+       know something that is not entirely obvious by looking at the code.
+    */
+    /* Requesting User to enter another number */
+    printf("Enter another number: ");
+    scanf("%d", &y);
+    fgets(s,100,stdin);             // remove the \n left by scanf
+    printf("You entered %d\n", y);
+
+    /* Calculate the sum of the two numbers and print it out */
+    sum = x + y;                   // calculate the sum of x and y
+    printf("The sum of %d and %d is %d\n", x, y, sum);  // print the result
+
+    /* Pause the console so we can look at it before it vanishes */
+    fgets(s,100,stdin);
     return 0;
 }

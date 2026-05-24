f4b06d23e9ea78986a6b4392f16e2d1bc9ef4dfc
Sylfhen <mcleod.sy@northeastern.edu>
2026-05-24 16:09:26 -0700

    Added implementation for the computation and comments

---
+++ Day 2/Homework 2/radius.c
@@ -0,0 +1,25 @@
+/* This program accepts an integer radius and prints an integer approximation of the circle's area */
+
+/*Library Declarations*/
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int radius;
+    double pi = acos(-1);//the value of pi calculated in C
+
+    /* Prompt and validate integer radius */
+    printf("Enter an integer radius: ");
+    if (scanf("%d", &radius) != 1) {
+        printf("Invalid input. Please enter an integer.\n");
+        return 1;
+    }
+
+    /* Calculate area and round to nearest integer */
+    int area = (int)round(pi * radius * radius);
+
+    /* Print result matching expected format */
+    printf("For a circle of radius %d.0 the area is %d\n", radius, area);
+
+    return 0;
+}

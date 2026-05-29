047aa94e14274e80b1c7cb5be9fd0ec92ccb7f99
Sylfhen <mcleod.sy@northeastern.edu>
2026-05-29 12:30:19 -0700

    Implemented programme for computing complements

--- Day 3/Homework 3/complement.c
+++ Day 3/Homework 3/complement.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 
-int main () {
+/* This programme calculates the complement of nums by removing the rightmost digit */
+/* Recursively calling itself to complement the rest of nums           */
+/* then composing that result with 9 - digit                           */
+
+int FindingComplement(int nums) {
+    if (nums == 0) {
+        return 0;
+    }
+    if (nums < 0) {
+        return -1;
+    }
+    return (FindingComplement(nums / 10) * 10) + (9 - (nums % 10));
+}
+
+int main() {
     char s[100];
+    int nums;
 
+    printf("Enter your number: ");
+    if (scanf("%d", &nums) != 1) {
+        printf("Invalid input\n");
+        fgets(s, 100, stdin);   // pause before closing on invalid input
+        return 1;
+    }
 
+    getchar();                  // consume the leftover '\n' from scanf
 
-    fgets(s,100,stdin);
+    if (nums < 0) {
+        printf("Error: negative numbers are not supported\n");
+    } else if (nums == 0) {
+        printf("The complement is 9\n");
+    } else {
+        printf("The complement of %d is %d\n", nums, FindingComplement(nums));
+    }
+
+    printf("Press Enter to exit...\n");
+    fgets(s, 100, stdin);       // now actually waits for Enter
     return 0;
 }

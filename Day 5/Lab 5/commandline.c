7612d49d8ed843942243b50a7717a39c9b0ccfc3
Sylfhen <mcleod.sy@northeastern.edu>
2026-06-11 12:21:03 -0700

    implemented command line

--- Day 5/Lab 5/commandline.c
+++ Day 5/Lab 5/commandline.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char **argv) {
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
 
-    return 0;
+if (argc > 1) {
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("Error opening file: %s\n", argv[1]);
+        return 1;
+    }
+    printf("File opened successfully: %s\n", argv[1]);
+    fclose(file);
 }
+
+return 0;

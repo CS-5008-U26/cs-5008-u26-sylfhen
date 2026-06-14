9d9baf003daf9d0af7060d0111320f997b0f23ad
Sylfhen <mcleod.sy@northeastern.edu>
2026-06-13 22:00:05 -0700

    Making changes to implementation and recompiling

--- Day 5/Homework 5/printCsv.c
+++ Day 5/Homework 5/printCsv.c
@@ -65,7 +65,7 @@ int main(void) {
 
 /* Check if the file was opened successfully */    
     if (file == NULL) {
-        printf("\nError opening file: %s\n", filename);
+        printf("Error opening file: %s\n", filename);
         return 1;
     }
 

af42cf622162d303115e0a89bbf3b31a2e6b2d75
Sylfhen <mcleod.sy@northeastern.edu>
2026-06-25 11:20:28 -0700

    going through the code

--- Day 7/Lab 7/primes.c
+++ Day 7/Lab 7/primes.c
@@ -7,24 +7,25 @@
 int main() {
 
     clock_t start = clock();
+    /*double timeLimit = 0.5;*/
 
     long long int num = 2;/* Start checking for primes from 2 */
     long long int lastPrime = 2;/*intialize starts at 2 since 2 is the first prime*/
 
     while (1) {
 
-        int prime = 1;
+        int isprime = 1;
         long long int limit = (long long int)sqrt((double)num);
 
         /*d is the divisor we are checking */
         for (long long int d = 2; d <= limit; d++) {
             if (num % d == 0) {
-                prime = 0;
+                isprime = 0;
                 break;
             }
         }
 
-        if (prime) {
+        if (isprime) {
 
             lastPrime = num;
 

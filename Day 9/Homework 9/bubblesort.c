5be5227da925ce6e9b9dc246127760a03cf10ed7
Sylfhen <mcleod.sy@northeastern.edu>
2026-07-12 11:47:11 -0700

    Adjusting the implementation of the bubblesort for better efficiency

--- Day 9/Homework 9/bubblesort.c
+++ Day 9/Homework 9/bubblesort.c
@@ -173,8 +173,9 @@ int main () {
     int keepgoing = 1;
     while (keepgoing) {
         printf ("How many elements? ");
-        fgets(buffer, 100, stdin);
-        if (buffer[0] == '\n') {
+        if (fgets(buffer, 100, stdin) == NULL) {
+            keepgoing = 0;
+        } else if (buffer[0] == '\n') {
             keepgoing = 0;
         } else {
             nelements = atoi(buffer);
@@ -194,8 +195,9 @@ int main () {
     keepgoing = 1;
     while (keepgoing) {
         printf ("How many elements (sorted data)? ");
-        fgets(buffer, 100, stdin);
-        if (buffer[0] == '\n') {
+        if (fgets(buffer, 100, stdin) == NULL) {
+            keepgoing = 0;
+        } else if (buffer[0] == '\n') {
             keepgoing = 0;
         } else {
             nelements = atoi(buffer);
@@ -215,8 +217,9 @@ int main () {
     keepgoing = 1;
     while (keepgoing) {
         printf ("How many elements (reverse sorted data)? ");
-        fgets(buffer, 100, stdin);
-        if (buffer[0] == '\n') {
+        if (fgets(buffer, 100, stdin) == NULL) {
+            keepgoing = 0;
+        } else if (buffer[0] == '\n') {
             keepgoing = 0;
         } else {
             nelements = atoi(buffer);
